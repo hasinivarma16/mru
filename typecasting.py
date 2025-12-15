d685cf6d181f3333b40132659dd7653387ada18c
Hasini Varma <hasinivarma16@gmail.com>
2025-12-15 14:56:43 +0530

    add

---
+++ typecasting.py
@@ -0,0 +1,13 @@
+# type casting: converting one datatype to another datatype
+#print(int("10"))
+#print(int(10))
+print(int("10"))
+print(int(10), type(int(10)))
+#converting str to float
+print(float("10"))
+print(float("1000.5"))
+#converting str to complex
+print(complex("10"))
+print(complex("10.5"))
+print(complex(100,5))
+print(complex(True,False))
